cf6bac00cb2a05b005cb7539136a962f845c2db7
Bruno Santos <bruno1pb13@gmail.com>
2023-02-21 10:53:23 -0300

    fix: qualquer tecla tira o jogo do pause

--- games/snake/utils/keybind.js
+++ games/snake/utils/keybind.js
@@ -1,18 +1,21 @@
 function keyPressed() {
-    updateState('play')
 
     switch (keyCode){
         case LEFT_ARROW:
             if(direction != 'right') direction = 'left'
+            updateState('play')
         break;
         case RIGHT_ARROW:
             if(direction != 'left') direction = 'right'
+            updateState('play')
         break;
         case UP_ARROW:
             if(direction != 'bottom') direction = 'top'
+            updateState('play')
         break;
         case DOWN_ARROW:
             if(direction != 'top') direction = 'bottom'
+            updateState('play')
         break;
         case ESCAPE || BACKSPACE:
             direction = null
